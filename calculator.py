0d9fa997cc2e494d1ff1988dc4a28a7f1cda9490
ArturKashafutdinov <kashafutdinov.ar@ufl.edu>
2025-04-14 12:04:42 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,32 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a,b):
+    try:
+        c = a / b
+        return c
+    except ZeroDivisionError as e:
+        print(e)
+
+def logarithm(a,b):
+    try:
+        c = math.log(b,a)
+        return c
+    except ValueError as e:
+        print(e)
+
+def exponent(a,b):
+    return a ** b
+
+
 
 
